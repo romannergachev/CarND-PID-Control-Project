d8e7eb05f432b1f04e71cf3d42a1a446ca30eab0
Roman Nergachev <nergachev.roman@gmail.com>
2017-04-30 16:52:57 +0300

    feat: Reformat main, change params

--- src/main.cpp
+++ src/main.cpp
@@ -21,26 +21,31 @@ std::string hasData(std::string s) {
   auto b2 = s.find_last_of("]");
   if (found_null != std::string::npos) {
     return "";
-  }
-  else if (b1 != std::string::npos && b2 != std::string::npos) {
+  } else if (b1 != std::string::npos && b2 != std::string::npos) {
     return s.substr(b1, b2 - b1 + 1);
   }
   return "";
 }
 
-int main()
-{
+int main() {
   uWS::Hub h;
 
-  PID pid;
-  // TODO: Initialize the pid variable.
+  //init steering PID
+  PID steeringPid;
+  //steeringPid.Init(0.15, 0.0001, 2.0); speed = 30
+  //steeringPid.Init(0.15, 0.0001, 2.0); speed = 40
+  //steeringPid.Init(0.15, 0.001,  3.0); speed = 50
+  steeringPid.Init(0.15, 0.001, 3.0);
 
-  h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
+  //init speed PID
+  PID speedPid;
+  speedPid.Init(0.2, 0.001, 2.0);
+
+  h.onMessage([&steeringPid, &speedPid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
-    if (length && length > 2 && data[0] == '4' && data[1] == '2')
-    {
+    if (length && length > 2 && data[0] == '4' && data[1] == '2') {
       auto s = hasData(std::string(data));
       if (s != "") {
         auto j = json::parse(s);
@@ -50,23 +55,37 @@ int main()
           double cte = std::stod(j[1]["cte"].get<std::string>());
           double speed = std::stod(j[1]["speed"].get<std::string>());
           double angle = std::stod(j[1]["steering_angle"].get<std::string>());
-          double steer_value;
+
           /*
-          * TODO: Calcuate steering value here, remember the steering value is
-          * [-1, 1].
-          * NOTE: Feel free to play around with the throttle and speed. Maybe use
-          * another PID controller to control the speed!
-          */
-          
+           * Calcuate steering value here, remember the steering value is [-1, 1].
+           */
+          steeringPid.UpdateError(cte);
+          double steeringValue = steeringPid.TotalError();
+
+          if (steeringValue < -1.0) {
+            //std::cout << "Speed Value Fixed! < " << steeringValue << std::endl;
+            steeringValue = -1.0;
+          } else if (steeringValue > 1.0) {
+            //std::cout << "Speed Value Fixed! > " << steeringValue << std::endl;
+            steeringValue = 1.0;
+          }
+
+          /*
+           * Calcuate speed value here, remember the speed value is [30, 100].
+           */
+          speedPid.UpdateError(speed - speedPid.MINIMUM_SPEED);
+          double speedValue = speedPid.TotalError();
+
           // DEBUG
-          std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
+          //std::cout << "CTE: " << cte << " Steering Value: " << steeringValue << std::endl;
 
           json msgJson;
-          msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = 0.3;
+          msgJson["steering_angle"] = steeringValue;
+          msgJson["throttle"]       = speedValue;
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
-          std::cout << msg << std::endl;
+          //std::cout << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+
         }
       } else {
         // Manual driving
@@ -80,12 +99,9 @@ int main()
   // doesn't compile :-(
   h.onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data, size_t, size_t) {
     const std::string s = "<h1>Hello world!</h1>";
-    if (req.getUrl().valueLength == 1)
-    {
+    if (req.getUrl().valueLength == 1) {
       res->end(s.data(), s.length());
-    }
-    else
-    {
+    } else {
       // i guess this should be done more gracefully?
       res->end(nullptr, 0);
     }
@@ -101,12 +117,9 @@ int main()
   });
 
   int port = 4567;
-  if (h.listen(port))
-  {
+  if (h.listen(port)) {
     std::cout << "Listening to port " << port << std::endl;
-  }
-  else
-  {
+  } else {
     std::cerr << "Failed to listen to port" << std::endl;
     return -1;
   }
