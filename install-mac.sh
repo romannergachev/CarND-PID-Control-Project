4a018a4f1637be90fe7ebd4f469d27ec293e7996
Roman Nergachev <nergachev.roman@gmail.com>
2017-04-30 11:37:38 +0300

    fix: Fix Mac install openssl

--- install-mac.sh
+++ install-mac.sh
@@ -1,3 +1,4 @@
+cmake -DOPENSSL_ROOT_DIR=/usr/local/ssl -DOPENSSL_LIBRARIES=/usr/local/ssl/lib
 brew install openssl libuv cmake
 git clone https://github.com/uWebSockets/uWebSockets 
 cd uWebSockets
@@ -6,6 +7,7 @@ patch CMakeLists.txt < ../cmakepatch.txt
 mkdir build
 export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig 
 cd build
+OPENSSL_ROOT_DIR=/usr/local/opt/openssl/
 cmake ..
 make 
 sudo make install
